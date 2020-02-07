a7475e9d46720c95c2c1ac2a5b699bfb60d8aa31
David <davidpluseipi@librem.one>
2020-02-07 12:59:28 -0800

    mods to adventure.cpp

--- adventure.cpp
+++ adventure.cpp
@@ -1,18 +1,33 @@
 //Adventure
 //by David Meissner
 
-#include <iostream>
-#include <string>
+#include <iostream> // cin, cout
+#include <string> // string datatype
+#include <sstream> // ss.str()
 using namespace std;
 
 int main() {
   // variable declaration / definition
-  string userInput = "";
+  char userInput[50] = " ";
+  string userInputStr = " ";
+  //stringstream ss; // was used when testing stringstream use
 
-  cout << "You are standing on the side of a deserted road.";
+  // user commands
+  string cmd001 = "wait for bus";
+
+  cout << "You are standing on the side of a deserted road." << endl;
   cin >> userInput;
+  cout << userInput;
+  //userInputStr = ss.str();
+  userInputStr = userInput;
+  cout << userInputStr << endl;
 
-  if (userInput == "wait for bus") {
-    cout << "a bus has arrived";
+  if (!userInputStr.compare(cmd001)) {
+    cout << "A bus has arrived.";
+  } else {
+    cout << "I don't understand that." << endl;
   }
+
+  return 0;
+
 }
