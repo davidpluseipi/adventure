27217e02efdc542a3424bc525b60715f17a72565
David <davidpluseipi@librem.one>
2020-02-07 12:27:32 -0800

    renaming main as adventure; adding .atom-build.yml

--- main.cpp
+++ main.cpp
@@ -1,2 +1,18 @@
-/Adventure
-/by David Meissner
+//Adventure
+//by David Meissner
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  // variable declaration / definition
+  string userInput = "";
+
+  cout << "You are standing on the side of a deserted road.";
+  cin >> userInput;
+
+  if (userInput == "wait for bus") {
+    cout << "a bus has arrived";
+  }
+}
